9ea535ff2cd0c415a03cb8eb0728eb426e5af8a4
Terence Liu <s8854086@gmail.com>
2021-09-18 18:15:32 +0800

    add all my files

---
+++ HelloWorld.js
@@ -0,0 +1,12 @@
+// WinDBG JavaScript sample
+// Prints Hello World
+
+function initializeScript()
+{
+  host.diagnostics.debugLog("***> Hello World\n");
+}
+
+function addTwoValues( a,b)
+{
+  return a+b;
+}
